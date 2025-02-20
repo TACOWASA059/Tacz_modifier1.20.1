0d650db7e59021b14c95ef7d8380ac1ee156ca2d
BuildTools <unconfigured@null.spigotmc.org>
2025-02-20 18:42:09 +0900

    1.0.0

---
+++ src/main/java/com/github/tacowasa059/taczmodifer/Tacz_modifer.java
@@ -0,0 +1,49 @@
+package com.github.tacowasa059.taczmodifer;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Tacz_modifer.MODID)
+public class Tacz_modifer {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "tacz_modifer";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    @SuppressWarnings("removal")
+    public Tacz_modifer() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
---
+++ src/main/java/com/github/tacowasa059/taczmodifer/mixin/EntityRenderDispatcherMixin.java
@@ -0,0 +1,123 @@
+package com.github.tacowasa059.taczmodifer.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import com.tacz.guns.api.TimelessAPI;
+import com.tacz.guns.client.model.BedrockAmmoModel;
+import com.tacz.guns.client.renderer.entity.EntityBulletRenderer;
+import com.tacz.guns.client.resource.InternalAssetLoader;
+import com.tacz.guns.client.resource.index.ClientGunIndex;
+import com.tacz.guns.entity.EntityKineticBullet;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import net.minecraft.ReportedException;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Mixin(EntityRenderDispatcher.class)
+public abstract class EntityRenderDispatcherMixin {
+    @Shadow
+    public abstract  <T extends Entity> EntityRenderer<? super T> getRenderer(T p_114383_);
+    @Inject(method="render",at=@At("HEAD"),cancellable = true)
+    public <E extends Entity> void render(E p_114385_, double x, double y, double z, float yaw, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int p_114393_, CallbackInfo ci) {
+        if(p_114385_ instanceof EntityKineticBullet bullet){
+            EntityRenderer<? super E> entityrenderer = this.getRenderer(p_114385_);
+
+            try {
+                Vec3 vec3 = entityrenderer.getRenderOffset(p_114385_, partialTicks);
+                double d2 = x + vec3.x();
+                double d3 = y + vec3.y();
+                double d0 = z + vec3.z();
+                poseStack.pushPose();
+                poseStack.translate(d2, d3, d0);
+                GunRender(bullet, partialTicks, poseStack, bufferSource, p_114393_);
+
+                poseStack.popPose();
+            } catch (Throwable throwable) {
+                CrashReport crashreport = CrashReport.forThrowable(throwable, "Rendering entity in world");
+                CrashReportCategory crashreportcategory = crashreport.addCategory("Entity being rendered");
+                p_114385_.fillCrashReportCategory(crashreportcategory);
+                CrashReportCategory crashreportcategory1 = crashreport.addCategory("Renderer details");
+                crashreportcategory1.setDetail("Assigned renderer", entityrenderer);
+                crashreportcategory1.setDetail("Rotation", yaw);
+                crashreportcategory1.setDetail("Delta", partialTicks);
+                throw new ReportedException(crashreport);
+            }
+            ci.cancel();
+        }
+    }
+    @Unique
+    private static void GunRender(EntityKineticBullet bullet, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+        ResourceLocation gunId = bullet.getGunId();
+        Optional<ClientGunIndex> optionalClientGunIndex = TimelessAPI.getClientGunIndex(gunId);
+        if (!optionalClientGunIndex.isEmpty()) {
+            Optional var10000 = bullet.getTracerColorOverride();
+            ClientGunIndex var10001 = optionalClientGunIndex.get();
+            Objects.requireNonNull(var10001);
+            float[] tracerColor = (float[])var10000.orElseGet(var10001::getTracerColor);
+            ResourceLocation ammoId = bullet.getAmmoId();
+            TimelessAPI.getClientAmmoIndex(ammoId).ifPresent((ammoIndex) -> {
+                BedrockAmmoModel ammoEntityModel = ammoIndex.getAmmoEntityModel();
+                ResourceLocation textureLocation = ammoIndex.getAmmoEntityTextureLocation();
+                if (ammoEntityModel != null && textureLocation != null) {
+                    poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTicks, bullet.yRotO, bullet.getYRot()) - 180.0F));
+                    poseStack.mulPose(Axis.XP.rotationDegrees(Mth.lerp(partialTicks, bullet.xRotO, bullet.getXRot())));
+                    poseStack.pushPose();
+                    poseStack.translate(0.0, 1.5, 0.0);
+                    poseStack.scale(-1.0F, -1.0F, 1.0F);
+                    ammoEntityModel.render(poseStack, ItemDisplayContext.GROUND, RenderType.entityTranslucentCull(textureLocation), packedLight, OverlayTexture.NO_OVERLAY);
+                    poseStack.popPose();
+                }
+
+//                if (bullet.isTracerAmmo()) {
+                float[] actualTracerColor = Objects.requireNonNullElse(tracerColor, ammoIndex.getTracerColor());
+                renderTracerAmmo(bullet, actualTracerColor, partialTicks, poseStack, packedLight);
+//                }
+
+            });
+        }
+    }
+    @Unique
+    private static void renderTracerAmmo(EntityKineticBullet bullet, float[] tracerColor, float partialTicks, PoseStack poseStack, int packedLight) {
+        EntityBulletRenderer.getModel().ifPresent((model) -> {
+            Entity shooter = bullet.getOwner();
+            if (shooter != null) {
+                poseStack.pushPose();
+                float width = 0.005F;
+                double trailLength = 0.85 * bullet.getDeltaMovement().length();
+
+                width *= bullet.getTracerSizeOverride() * 1.2;
+
+                poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTicks, bullet.yRotO, bullet.getYRot()) - 180.0F));
+                poseStack.mulPose(Axis.XP.rotationDegrees(Mth.lerp(partialTicks, bullet.xRotO, bullet.getXRot())));
+                poseStack.translate(0.0, -0.2, -trailLength / 2.0 -1f);
+                poseStack.scale(width, width, (float)trailLength);
+
+                RenderType type = RenderType.energySwirl(InternalAssetLoader.DEFAULT_BULLET_TEXTURE, 15.0F, 15.0F);
+
+                model.render(poseStack, ItemDisplayContext.NONE, type, packedLight, OverlayTexture.NO_OVERLAY,
+                        tracerColor[0], tracerColor[1], tracerColor[2], 1.0F);
+
+                poseStack.popPose();
+
+            }
+        });
+    }
+}
